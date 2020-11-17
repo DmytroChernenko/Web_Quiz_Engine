8999e7b93f78ccba705c310b259ae7ea1c9dc6f3
Dmytro <mitresko12@gmail.com>
2020-11-17 11:43:15 +0200

    simple QuizDAO implementation adn QuizNotFoundException

--- Web Quiz Engine/task/src/dao/QuizDAO.java
+++ Web Quiz Engine/task/src/dao/QuizDAO.java
@@ -1,4 +1,25 @@
 package dao;
 
+import entity.Quiz;
+import exceptions.QuizNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizDAO {
+
+    List<Quiz> quizzes = new ArrayList<>();
+
+    public Quiz getQuizById(int id) throws QuizNotFoundException {
+        try {
+            return quizzes.get(id);
+        } catch (Exception e) {
+            throw new QuizNotFoundException();
+        }
+    }
+
+    public List<Quiz> getAll() {
+        return quizzes;
+    }
+
 }
